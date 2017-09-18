c35bb7c924b86317253a189de59b0eb532c6bcb1
pepesan <pepesan@gmail.com>
2017-09-18 13:42:02 +0200

    Commit inicial poca cosa, index y arrays

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Online PHP-7 Script Execution</title>
+</head>
+
+<body>
+
+<?php
+echo "<h1>Hello, PHP-7!</h1>";
+?>
+
+<ul>
+    <li><a href="arrays.php">Arrays</a></li>
+</ul>
+</body>
+</html>
